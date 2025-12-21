71e6f6c1cc462ad69fb7e0055e2bd7b014548f26
PraveenChandraBoseSN <praveenchandrabose.sn@gmail.com>
2025-12-21 18:56:47 +0530

    Moved the root files to the respective folder

--- client_request.py
+++
@@ -1,8 +0,0 @@
-import socket
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-ip = "127.0.0.1"
-prt = 5000
-add = (ip, prt)
-client.connect(add)
-client.send("Hello Server!".encode())
-client.close()
