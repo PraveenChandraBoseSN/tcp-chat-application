7057975f82ad8dd606ff40b90c6abdc216b1a31a
PraveenChandraBoseSN <praveenchandrabose.sn@gmail.com>
2025-12-21 18:29:30 +0530

    Receive message from the client #3

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 import socket
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 ip = socket.gethostbyname(socket.gethostname())
 port = 5000
 addr = (ip, port)
